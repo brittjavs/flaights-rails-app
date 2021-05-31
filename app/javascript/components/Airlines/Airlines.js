b70af2e2d73da87f5a6c2df594d2c5ff262d117d
Brittany Javalera <brittjavalera@gmail.com>
2021-05-31 15:29:33 -0700

    Add style cmponents to Airline and Airlines components.

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,6 +1,30 @@
 import React, {useState, useEffect, Fragment} from 'react'
 import axios from 'axios'
 import Airline from '../Airline/Airline'
+import styled from 'styled-components'
+
+const Home = styled.div`
+text-align: center;
+max-widht: 1200px;
+margin-left: auto;
+margin-right: auto;`
+
+const Header = styled.div`
+padding: 100px 100px 10px 100px;
+h1{
+    font-size: 40px;
+}`
+
+const Subheader = styled.div`
+font-weight: 300;
+font-size: 26px;`
+
+const Grid = styled.div`
+display: grid;
+grid-template-columns: repeat(4, 1fr);
+grid-gap: 20px;
+width: 100%;
+PADDING: 20px;`
 
 const Airlines = () => {
     const [airlines, setAirlines] = useState([])
@@ -8,9 +32,7 @@ const Airlines = () => {
         //get all airlines from api
         //update airlines in state
         axios.get('/api/v1/airlines.json')
-        .then (resp => {
-            setAirlines(resp.data.data)
-        })
+        .then (resp => setAirlines(resp.data.data))
         .catch( resp => console.log(resp))
     }, [airlines.length])
 
@@ -24,15 +46,15 @@ const Airlines = () => {
     })
 
     return (
-        <div className="Home">
-            <div className="header"></div>
+        <Home>
+            <Header>
                 <h1>Open Flights</h1>
-            <div className="subheader">Honest, unbiased airline reviews.</div>
-            <div className="grid">
-                <ul>{grid}</ul>
-
-            </div>
-        </div>
+                <Subheader>Honest, unbiased airline reviews.</Subheader>
+            </Header>
+            <Grid>
+                {grid}
+            </Grid>
+        </Home>
     
     )
 }
