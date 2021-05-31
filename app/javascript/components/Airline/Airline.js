e926208f3d90b1a7da0826b1082b2cdf4660d79d
Brittany Javalera <brittjavalera@gmail.com>
2021-05-31 15:07:42 -0700

    Create Airline component and use props to populate div that is being returned by Airline component. Import Ailrine in Airlines.js and return it in grid. Add avg_score to airline serializer.

--- app/javascript/components/Airline/Airline.js
+++ app/javascript/components/Airline/Airline.js
@@ -1,7 +1,18 @@
 import React from 'react'
 
-const Airline = () => {
-    return <div>This is Airline show for our app.</div>
+const Airline = (props) => {
+    return (
+        <div className="card">
+            <div className="airline-logo">
+                <img src={props.attributes.image_url} alt={props.attributes.name} />
+            </div>
+            <div className="airline-name">{props.attributes.name}</div>
+            <div className="airline-score">{props.attributes.avg_score}</div>
+            <div className="airline-link">
+                <a href={`/airlines/${props.attributes.slug}`}>View Ailrine</a>
+            </div>
+        </div>
+    )
 }
 
 export default Airline
--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect, Fragment} from 'react'
 import axios from 'axios'
+import Airline from '../Airline/Airline'
 
 const Airlines = () => {
     const [airlines, setAirlines] = useState([])
@@ -13,14 +14,25 @@ const Airlines = () => {
         .catch( resp => console.log(resp))
     }, [airlines.length])
 
-    const list = airlines.map( item => {
-        return <li key={item.attributes.name}>{item.attributes.name}</li>
+    const grid = airlines.map( item => {
+        return (
+            <Airline
+            key={item.attributes.name}
+            attributes={item.attributes}
+            />
+        )
     })
+
     return (
-        <Fragment>
-            <div>This is Airline index page</div>
-            <ul>{list}</ul>
-        </Fragment>
+        <div className="Home">
+            <div className="header"></div>
+                <h1>Open Flights</h1>
+            <div className="subheader">Honest, unbiased airline reviews.</div>
+            <div className="grid">
+                <ul>{grid}</ul>
+
+            </div>
+        </div>
     
     )
 }
