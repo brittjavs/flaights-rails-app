b5c5f8a8811a9e4e81f387a3f91e41215d7083c2
Brittany Javalera <brittjavalera@gmail.com>
2021-06-06 21:20:42 -0700

    Create setRating in Airline and pass it as prop to reviewForm. Add setRating prop to onClick event in reviewForm ratingOption label

--- app/javascript/components/Airline/Airline.js
+++ app/javascript/components/Airline/Airline.js
@@ -61,6 +61,11 @@ const Airline = (props) => {
         .catch(resp => {})
     }
 
+    const setRating = (score, e) => {
+        e.preventDefault()
+        setReview({...review, score})
+    }
+
     return (
         <Wrapper>
             {
@@ -79,6 +84,7 @@ const Airline = (props) => {
                         <ReviewForm
                         handleChange={handleChange}
                         handleSubmit={handleSubmit}
+                        setRating={setRating}
                         attributes={airline.data.attributes}
                         review={review}
                         />
